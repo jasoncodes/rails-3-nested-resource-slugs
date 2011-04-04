eb98d9e473451430778d9dc29759110d26af1d1a
Jason Weathered <jason@jasoncodes.com>
2011-04-04 16:45:04 +1000

    Move nested routes to their own parent block to ensure they render last.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Example::Application.routes.draw do
-  resources :accounts, :path => '' do
+  resources :accounts, :path => ''
+  resources :accounts, :path => '', :only => [] do
     resources :projects, :path => '', :except => [:index]
   end
 end
