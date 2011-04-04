5ccda59c6da10eabb81d51002ba0287069380950
Jason Weathered <jason@jasoncodes.com>
2011-04-04 16:14:41 +1000

    Failed attempt at removing controller names from routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Example::Application.routes.draw do
-  resources :accounts do
-    resources :projects
+  resources :accounts, :path => '' do
+    resources :projects, :path => '', :except => [:index]
   end
 end
